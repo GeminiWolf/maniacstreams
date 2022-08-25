16b4afb4fb657723c1103526612b36d4e9a9f5eb
mihledamoyi <mihle.damoyi@sovtech.com>
2022-08-25 18:04:02 +0200

    watching api page

--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data/movie.json');
+const data = require("../data/movie.json");
 const fetch = require("node-fetch");
-require('dotenv').config();
+require("dotenv").config();
 
-router.get('/trending', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_MOVIEAPI}`)
-        .then(json => json.json())
-        .then(data => { res.json(data.results) })
-        .catch(err => console.log(err))
+router.get("/trending", (req, res) => {
+	fetch(
+		`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_MOVIEAPI}`
+	)
+		.then((json) => json.json())
+		.then((data) => {
+			res.json(data.results);
+		})
+		.catch((err) => console.log(err));
 });
 
 // movies
-router.get('/movies/:page', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIEAPI}&language=en-US&page=${req.params.page}`)
-        .then(json => json.json())
-        .then(data => { res.json(data.results) })
-        .catch(err => console.log(err))
+router.get("/movies/:page", (req, res) => {
+	fetch(
+		`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIEAPI}&language=en-US&page=${req.params.page}`
+	)
+		.then((json) => json.json())
+		.then((data) => {
+			res.json(data.results);
+		})
+		.catch((err) => console.log(err));
 });
 
 // series
-router.get('/series/:page', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.REACT_APP_MOVIEAPI}&language=en-US&page=${req.params.page}`)
-        .then(json => json.json())
-        .then(data => { res.json(data.results) })
-        .catch(err => console.log(err))
+router.get("/series/:page", (req, res) => {
+	fetch(
+		`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.REACT_APP_MOVIEAPI}&language=en-US&page=${req.params.page}`
+	)
+		.then((json) => json.json())
+		.then((data) => {
+			res.json(data.results);
+		})
+		.catch((err) => console.log(err));
+});
+
+// series
+router.get("/watching/:show/:id", (req, res) => {
+	const { show, id } = req.params;
+
+	fetch(
+		`https://api.themoviedb.org/3/${show}/${id}?api_key=${process.env.REACT_APP_MOVIEAPI}&language=en-US`
+	)
+		.then((json) => json.json())
+		.then((data) => {
+			res.json(data);
+		})
+		.catch((err) => console.log(err));
 });
 
 // search
-router.get('/search/:searchTerm/:page', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_MOVIEAPI}&query=${req.params.searchTerm}&page=${req.params.page}`)
-        .then(json => json.json())
-        .then(data => { res.json(data.results) })
-        .catch(err => res.json(err))
+router.get("/search/:searchTerm/:page", (req, res) => {
+	fetch(
+		`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_MOVIEAPI}&query=${req.params.searchTerm}&page=${req.params.page}`
+	)
+		.then((json) => json.json())
+		.then((data) => {
+			res.json(data.results);
+		})
+		.catch((err) => res.json(err));
 });
-
 
 module.exports = router;
